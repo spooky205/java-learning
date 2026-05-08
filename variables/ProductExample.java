07d2db8dbf610f6847e5602d6e945257f848e519
spooky205 <rdkaic1@gmail.com>
2026-05-08 09:48:33 -0300

    Create ProductExample.java
    Add product variables example

---
+++ variables/ProductExample.java
@@ -0,0 +1,15 @@
+public class ProductExample {
+
+    public static void main(String[] args) {
+
+        String produto = "camisa";
+        int preco = 30;
+        int quantidade = 3;
+        int valor = preco * quantidade;
+
+        System.out.println("produto: " + produto);
+        System.out.println("preco: " + preco);
+        System.out.println("quantidade: " + quantidade);
+        System.out.println("valor: " + valor);
+    }
+}
