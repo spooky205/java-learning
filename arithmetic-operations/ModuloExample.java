7a0907cdb9cca12963131f84432e33c60574d1c6
spooky205 <rdkaic1@gmail.com>
2026-05-08 09:56:47 -0300

    Add ModuloExample class to demonstrate modulo operation
    Add modulo operator example

---
+++ arithmetic-operations/ModuloExample.java
@@ -0,0 +1,16 @@
+public class ModuloExample {
+
+    public static void main(String[] args) {
+
+        int a = 9;
+        int b = 9 % 2;
+        int c = 4;
+        int d = 4;
+
+        System.out.println("a=" + a);
+        System.out.println("b=" + b);
+        System.out.println("c=" + c);
+        System.out.println("d=" + d);
+
+    }
+}
