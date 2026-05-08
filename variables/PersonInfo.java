47393543f5a673b8d21e73f8311804cf75e5a328
spooky205 <rdkaic1@gmail.com>
2026-05-08 10:19:25 -0300

    Add PersonInfo class with personal details
    Add person information variables exercise

---
+++ variables/PersonInfo.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        String name = "Kaic";
+        int age = 21;
+        double height = 1.81;
+
+        System.out.println("name: " + name);
+        System.out.println("age: " + age);
+        System.out.println("height: " + height);
+
+    }
+}
