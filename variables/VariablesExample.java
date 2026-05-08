4c3a3b07dd7208ceb6d689b9c9cef2741671e963
spooky205 <rdkaic1@gmail.com>
2026-05-08 09:43:07 -0300

    Create VariablesExample.java
    Exercises and examples created while learning Java.

---
+++ variables/VariablesExample.java
@@ -0,0 +1,11 @@
+public class VariablesExample {
+    public static void main(String[] args) {
+
+        int age = 20;
+        String name = "Kai";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+
+    }
+}
