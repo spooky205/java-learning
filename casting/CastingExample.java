df5be7e3f761138d48094167b34d1d38d24e31b7
spooky205 <rdkaic1@gmail.com>
2026-05-18 18:52:13 -0300

    Add CastingExample.java for type casting demonstration
    Simple Java program demonstrating type casting between data types.

---
+++ casting/CastingExample.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int number = 10;
+        double result = number;
+
+        System.out.println("number= " + result);
+
+    }
+}
