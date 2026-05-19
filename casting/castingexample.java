ddf023b4cbd5fd839ab17f989ed8b33d35fdf0c4
spooky205 <rdkaic1@gmail.com>
2026-05-19 11:48:57 -0300

    Add casting example in castingexample.java

---
+++ casting/castingexample.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        double number = 9.78;
+        int result = (int) number;
+
+        System.out.println("number = " + result);
+
+    }
+}
