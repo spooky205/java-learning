ec0c1f1db06d89a564e0605f6f559710421075de
spooky205 <rdkaic1@gmail.com>
2026-05-19 12:05:02 -0300

    Add casting example in castingexample3.java
    Simple Java exercise demonstrating manual casting from double to int and performing a basic sum after the conversion.

---
+++ casting/castingexample3.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        double numero1 = 5.7;
+        double numero2 = 3.2;
+        int numero3 = (int) numero1;
+
+        int numero4 = (int) numero2;
+        int result = numero3 + numero4;
+
+        System.out.println("result: "+ result);
+
+    }
+}
