c7f6f1732f02ce252840179f8ba65083082e6f13
spooky205 <rdkaic1@gmail.com>
2026-05-25 18:52:42 -0300

    Add StringComparison class to compare strings
    This Java project demonstrates how to compare Strings using the `.equals()` method.
    
    The program compares two different Strings, "Java" and "java", and prints the result to the console using a boolean variable. It helps beginners understand how String comparison works in Java and how uppercase and lowercase letters affect the comparison result.
    
    Concepts practiced in this project:
    - String declaration
    - String comparison with `.equals()`
    - Boolean values
    - Console output using `System.out.println()`
    - Case-sensitive comparison in Java

---
+++ operations /StringComparison.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String str1 = "Java";
+        String str2 = "java";
+
+        boolean result = str1.equals(str2);
+
+        System.out.println("result: "+ result);
+    }
+}
